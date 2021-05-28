d6035e0c0e3074db533161ed20d7b93bf4da17a2
Gustavo Bezerras <gustavobs16@gmail.com>
2021-05-28 01:31:03 -0400

    feat: Interação com o usuário

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,10 +5,7 @@ import entities.*;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.InputMismatchException;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Program {
@@ -18,10 +15,11 @@ public class Program {
 
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
         Scanner sc = new Scanner(System.in);
-        Calendario calendario = new Calendario("CAMPO_GRANDE", "MS", "2021");
+
+
         Rh rh = new Rh();
 
-        Funcionario f1 = new FuncionarioClt("05258539252", "Gustavo", sdf1.parse("17/06/1994"), "Rua longedemais");
+        Funcionario f1 = new FuncionarioClt("11111222222", "Gustavo", sdf1.parse("17/06/1994"), "Rua longedemais");
         Funcionario f2 = new FuncionarioDiarista("05258539254", "Danilo", sdf1.parse("08/02/1999"), "Rua longedemais");
         Funcionario f3 = new FuncionarioClt("05258539251", "Filipe", sdf1.parse("23/04/1990"), "Rua longedemais");
         Funcionario f4 = new FuncionarioPj("13258149000117", "João", sdf1.parse("31/12/2000"), "Rua longedemais");
@@ -35,6 +33,7 @@ public class Program {
         System.out.println("Bem vindo ao Gerenciador de Funcionários!");
         System.out.println("Você possui 5 funcionários cadastrados em sua empresa");
 
+        loop:
         while (true) {
             int acao = 0;
             System.out.println();
@@ -42,15 +41,19 @@ public class Program {
             System.out.println("(1) Adicionar novo funcionário CLT");
             System.out.println("(2) Adicionar novo funcionário PJ");
             System.out.println("(3) Adicionar novo funcionário diarista");
-            System.out.println("(4) Imprimir relatórios de escala");
-            System.out.println("(5) Imprimir relatórios pagamento");
-            System.out.println("(6) Finalizar Programa");
+            System.out.println("(4) Imprimir escala");
+            System.out.println("(5) Imprimir relatórios de pagamento em ordem crescente");
+            System.out.println("(6) Imprimir relatórios de pagamento em ordem decrescente");
+            System.out.println("(7) Pagar Funcionários");
+            System.out.println("(8) Finalizar Programa");
             System.out.print("Digite um número: ");
 
-            if (sc.hasNextInt())
+            if (sc.hasNextInt()) {
                 acao = sc.nextInt();
+                sc.nextLine();
+            }
             else {
-                System.out.print("Opção inválida. ");
+                System.err.print("Opção inválida.\n");
                 sc.nextLine();
             }
             switch (acao){
@@ -60,29 +63,32 @@ public class Program {
                     rh.addFunc(ColetaDados(acao));
                     break;
                 case(4):
-                    System.out.print("Digite a data de um domingo para a qual deseja saber a escala semanal (DD/MM/AAAA): ");
-                    Date dataEscala = sdf1.parse(sc.next());
-                    sc.nextLine();
-                    rh.gerarEscala(dataEscala, calendario.getFeriados());
+                    escalas(rh);
                     break;
                 case(5):
+                    rh.relatorioTotal("crescente");
+                    System.out.println("\nTecle ENTER para voltar ao menu principal");
+                    sc.nextLine();
                     break;
+                case(6):
+                    rh.relatorioTotal("decrescente");
+                    System.out.println("\nTecle ENTER para voltar ao menu principal");
+                    sc.nextLine();
+                    break;
+                case(7):
+                    rh.pagarFuncionarios();
+                    System.out.println("\nTecle ENTER para voltar ao menu principal");
+                    sc.nextLine();
+                    break;
+                case(8):
+                    break loop;
             }
-            if (acao == 6) {
-                break;
-            }
-        }
 
-        rh.gerarEscala(sdf1.parse("13/06/2021"), calendario.getFeriados());
-//        rh.imprimirEscala();
-//        rh.imprimirEscalaTipo("class entities.FuncionarioClt"); // No programa do usuário isso tem que ser mantido para funcionar, alternado apenas o nome final da classe
-//        rh.imprimirEscalaFunc("13258149000118");
-//        rh.relatorioTotal('c');
-//        rh.relatorioTotal('d');
-//        rh.pagarFuncionario();
+            System.out.println();
+        }
     }
 
-    static Funcionario ColetaDados(int tipoFuncionario) throws ParseException {
+    public static Funcionario ColetaDados(int tipoFuncionario) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Scanner input = new Scanner(System.in);
 
@@ -108,4 +114,87 @@ public class Program {
         return null;
     }
 
+    public static void escalas(Rh rh) throws ParseException, IOException, InterruptedException {
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar data = Calendar.getInstance();
+        Calendario calendario = new Calendario("CAMPO_GRANDE", "MS", "2021");
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+
+        if (!rh.getEscalaGerada()) {
+            System.out.println("A escala semanal ainda não foi gerada, deseja gerá-la?");
+            System.out.println("Digite \"sim\" para gerar ou tecle ENTER para sair deste menu: ");
+
+            if (sc.nextLine().equals("sim")) {
+                System.out.print("Digite a data para a qual deseja saber a escala semanal (DD/MM/AAAA): ");
+                Date dataEscala = sdf1.parse(sc.next());
+                data.setTime(dataEscala);
+                if(data.get(Calendar.DAY_OF_WEEK) > 1){
+                    int diasAmais = data.get(Calendar.DAY_OF_WEEK) - 1;
+                    data.add(Calendar.DATE, -diasAmais);
+                    dataEscala = data.getTime();
+                }
+                sc.nextLine();
+
+                rh.gerarEscala(dataEscala, calendario.getFeriados());
+                System.out.println("\nTecle ENTER para voltar ao menu principal");
+                sc.nextLine();
+            }
+            return;
+
+        }
+
+        while (true) {
+            System.out.println();
+            System.out.println("Digite um número de 1-5 para acessar uma opção do menu");
+            System.out.println("(1) Imprimir escala geral");
+            System.out.println("(2) Imprimir escala de funcionários CLT");
+            System.out.println("(3) Imprimir escala de funcionários PJ");
+            System.out.println("(4) Imprimir escala de funcionários Diaristas");
+            System.out.println("(5) Imprimir escala de um funcionário específico");
+            System.out.println("(6) Sair");
+            System.out.print("Digite um número: ");
+
+            if (sc.hasNextInt()) {
+                opcao = sc.nextInt();
+                sc.nextLine();
+                break;
+            }
+            else {
+                System.err.print("Opção inválida.\n");
+                System.out.println();
+                sc.nextLine();
+            }
+        }
+
+        switch(opcao){
+            case 1:
+                rh.imprimirEscala();
+                System.out.println("\nTecle ENTER para voltar ao menu principal");
+                sc.nextLine();
+                break;
+            case 2:
+                rh.imprimirEscalaTipo("class entities.FuncionarioClt");
+                System.out.println("\nTecle ENTER para voltar ao menu principal");
+                sc.nextLine();
+                break;
+            case 3:
+                rh.imprimirEscalaTipo("class entities.FuncionarioPj");
+                System.out.println("\nTecle ENTER para voltar ao menu principal");
+                sc.nextLine();
+                break;
+            case 4:
+                rh.imprimirEscalaTipo("class entities.FuncionarioDiarista");
+                System.out.println("\nTecle ENTER para voltar ao menu principal");
+                sc.nextLine();
+                break;
+            case 5:
+                System.out.println("Digite o CPF ou CNPJ do Funcionário: ");
+                rh.imprimirEscalaFunc(sc.next());
+                System.out.println("\nTecle ENTER para voltar ao menu principal");
+                sc.nextLine();
+                break;
+        }
+
+    }
 }
